581d9bcd11c23a0af5dca4ff1cda550d58c5cf3a
ReArmel <rearmel@icloud.com>
2023-04-17 20:19:38 -0400

    Começando a implementar um método que imprimi o tabuleiro

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 ﻿using tabuleiro;
+using xadrez_console;
 
-Posicao P;
-P = new Posicao(3, 4);
-
-Console.WriteLine("Posição: " + P); ;
+Tabuleiro tab = new Tabuleiro(8, 8);
+Tela.imprimirTabuleiro(tab);
 Console.ReadLine();
