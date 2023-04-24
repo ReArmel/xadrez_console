e4693236575503460995e04412d9cdcf31d3b81d
ReArmel <rearmel@icloud.com>
2023-04-24 09:55:07 -0400

    começando a colocar as peças

---
+++ xadrez/Torre.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using tabuleiro;
+
+namespace xadrez
+{
+    internal class Torre : Peca
+    {
+        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
