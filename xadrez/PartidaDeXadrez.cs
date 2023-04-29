91fb8da7360c1681920925eeede9a7e446c89d68
ReArmel <rearmel@icloud.com>
2023-04-29 16:51:36 -0400

    Correcao: mudando o nome do metodo

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -98,7 +98,7 @@ internal class PartidaDeXadrez
 
     public void validarPosicaoDeDestino(Posicao origem, Posicao destino)
     {
-        if (!tab.peca(origem).podeMoverPara(destino))
+        if (!tab.peca(origem).movimentoPossivel(destino))
         {
             throw new TabuleiroException("Posição de destino inválida");
         }
